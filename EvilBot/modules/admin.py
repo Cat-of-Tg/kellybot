b8996dad7c906ac3bc29d2bf3fbd94f1c782732c
teamLAD <87652538+teamLAD@users.noreply.github.com>
2021-07-25 20:26:37 +0530

    Update admin.py

--- EvilBot/modules/admin.py
+++ EvilBot/modules/admin.py
@@ -708,7 +708,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "Admin"
+__mod_name__ = "ᴀᴅᴍɪɴ"
 __command_list__ = [
     "adminlist",
     "admins",
