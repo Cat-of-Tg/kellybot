562ca6e32aaef6dc4c39606bf806793bf80cb558
teamLAD <87652538+teamLAD@users.noreply.github.com>
2021-07-25 22:28:25 +0530

    Update userinfo.py

--- EvilBot/modules/userinfo.py
+++ EvilBot/modules/userinfo.py
@@ -567,7 +567,7 @@ dispatcher.add_handler(GET_BIO_HANDLER)
 dispatcher.add_handler(SET_ABOUT_HANDLER)
 dispatcher.add_handler(GET_ABOUT_HANDLER)
 
-__mod_name__ = "Infos"
+__mod_name__ = "ɪɴғᴏ"
 __command_list__ = ["setbio", "bio", "setme", "me", "info"]
 __handlers__ = [
     ID_HANDLER,
