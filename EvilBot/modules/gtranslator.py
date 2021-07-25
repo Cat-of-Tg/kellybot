da680b5dced57996bd3a1a84d4fafee014b2630b
teamLAD <87652538+teamLAD@users.noreply.github.com>
2021-07-25 22:30:45 +0530

    Update gtranslator.py

--- EvilBot/modules/gtranslator.py
+++ EvilBot/modules/gtranslator.py
@@ -109,6 +109,6 @@ TRANSLATE_HANDLER = DisableAbleCommandHandler(["tr", "tl"], totranslate)
 
 dispatcher.add_handler(TRANSLATE_HANDLER)
 
-__mod_name__ = "G-Trans"
+__mod_name__ = "ᴛʀᴀɴsᴀʟᴀᴛᴇ"
 __command_list__ = ["tr", "tl"]
 __handlers__ = [TRANSLATE_HANDLER]
